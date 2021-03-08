e839c8d03f51c76a437a39fda81b442b5b096d43
Jumar Macato <hikari.netto23@gmail.com>
2021-03-08 11:40:29 +0800

    comment out d2d stuff

--- LottieSharp/WpfSurface/NativeMethods.cs
+++ LottieSharp/WpfSurface/NativeMethods.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Runtime.InteropServices;
-
-namespace LottieSharp.WpfSurface
-{
-    public static class NativeMethods
-    {
-        [DllImport("user32.dll", SetLastError = false)]
-        public static extern IntPtr GetDesktopWindow();
-    }
-}
+﻿// using System;
+// using System.Runtime.InteropServices;
+//
+// namespace LottieSharp.WpfSurface
+// {
+//     public static class NativeMethods
+//     {
+//         [DllImport("user32.dll", SetLastError = false)]
+//         public static extern IntPtr GetDesktopWindow();
+//     }
+// }
